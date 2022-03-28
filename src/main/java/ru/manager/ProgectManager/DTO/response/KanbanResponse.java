053629305c4c6da9f8b780dbc308d5a8f37994e7
aleksey <aleksey.k2000@mail.ru>
2022-03-28 22:22:21 +0700

    Убрана лишняя возвращаемая информация о канбане

--- src/main/java/ru/manager/ProgectManager/DTO/response/KanbanResponse.java
+++ src/main/java/ru/manager/ProgectManager/DTO/response/KanbanResponse.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import ru.manager.ProgectManager.entitys.Kanban;
 import ru.manager.ProgectManager.entitys.KanbanColumn;
-import ru.manager.ProgectManager.entitys.Project;
 
 import java.util.Comparator;
 import java.util.List;
@@ -15,9 +14,10 @@ import java.util.stream.Collectors;
 public class KanbanResponse {
     @Schema(description = "Список колонок канбана")
     private final List<KanbanColumnResponse> kanbanColumns;
+    @Schema(description = "Название канбана")
     private final String name;
+    @Schema(description = "Идентификатор канбана")
     private final long id;
-    private final Project project;
 
     public KanbanResponse(Kanban kanban, int pageIndexColumn, int countColumn, int pageIndexElement, int countElement){
         kanbanColumns = kanban.getKanbanColumns().stream()
@@ -28,6 +28,5 @@ public class KanbanResponse {
                 .collect(Collectors.toList());
         name = kanban.getName();
         id = kanban.getId();
-        project = kanban.getProject();
     }
 }
