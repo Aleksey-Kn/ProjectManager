73d9ce31fcd9ce227e07da75a6a33555a0f1a368
aleksey <aleksey.k2000@mail.ru>
2022-04-07 11:20:21 +0700

    Добавлена документация для TagRequest

--- src/main/java/ru/manager/ProgectManager/DTO/request/kanban/TagRequest.java
+++ src/main/java/ru/manager/ProgectManager/DTO/request/kanban/TagRequest.java
@@ -1,9 +1,13 @@
 package ru.manager.ProgectManager.DTO.request.kanban;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Запрос на добавление нового тега в канбан")
 public class TagRequest {
+    @Schema(description = "Текстовая составляющая тега")
     private String text;
+    @Schema(description = "Цветовая составляющая тега")
     private String color;
 }
