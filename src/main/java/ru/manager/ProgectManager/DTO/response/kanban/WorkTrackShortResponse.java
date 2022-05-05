1e97c51208f59fe3cd649a33d012eb8d833353a7
aleksey <aleksey.k2000@mail.ru>
2022-05-05 18:28:52 +0700

    Задокументирован WorkTrackShortResponse

--- src/main/java/ru/manager/ProgectManager/DTO/response/kanban/WorkTrackShortResponse.java
+++ src/main/java/ru/manager/ProgectManager/DTO/response/kanban/WorkTrackShortResponse.java
@@ -1,15 +1,21 @@
 package ru.manager.ProgectManager.DTO.response.kanban;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import ru.manager.ProgectManager.entitys.user.WorkTrack;
 
 import java.time.LocalDate;
 
 @Getter
+@Schema(description = "Данные об отработанном времени")
 public class WorkTrackShortResponse {
+    @Schema(description = "Дата, в которую была совершена работа")
     private final String workDate;
+    @Schema(description = "Количество времени, затраченное на задачу")
     private final int workTime;
+    @Schema(description = "Коментарий к выполненному объёму работы")
     private final String comment;
+    @Schema(description = "Идентификатор пользователя, выполнившего работу")
     private final long userId;
 
     public WorkTrackShortResponse(WorkTrack workTrack) {
