a37f74badb23f8d27ed1a5b07c3f13e2c9f45a8a
aleksey <aleksey.k2000@mail.ru>
2022-05-30 02:13:32 +0700

    Исправлена неправильная структура сущности Note

--- src/main/java/ru/manager/ProgectManager/entitys/user/Note.java
+++ src/main/java/ru/manager/ProgectManager/entitys/user/Note.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
@@ -12,6 +13,10 @@ import javax.persistence.Id;
 @Setter
 public class Note {
     @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
     private long userId;
 
     @Column(nullable = false)
