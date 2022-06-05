08e572f2c77a2bd10df33bf97c244700b59b9555
aleksey <aleksey.k2000@mail.ru>
2022-06-05 22:47:12 +0700

    Добавлена документация к ShortKanbanElementInfoList

--- src/main/java/ru/manager/ProgectManager/DTO/response/calendar/ShortKanbanElementInfoList.java
+++ src/main/java/ru/manager/ProgectManager/DTO/response/calendar/ShortKanbanElementInfoList.java
@@ -1,5 +1,6 @@
 package ru.manager.ProgectManager.DTO.response.calendar;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,6 +8,8 @@ import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
+@Schema(description = "Инфрмация о карточках, привязанных к указанному дню")
 public class ShortKanbanElementInfoList {
+    @Schema(description = "Список карточек, привязанных к указанному дню")
     private final List<ShortKanbanElementInfo> cards;
 }
